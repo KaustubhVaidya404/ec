8fa8fabce4fbe64bb62122fefe83ae1c342b4480
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2024-06-29 09:34:27 +0530

    primary get and post routes done in admin

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('test', views.dashboard, name='dashboard'), # testing url
     path('postproduct', views.postproduct, name='postproduct'), # post product url
     path('postcategory', views.postcategory, name='postcategory'), # post category url
+    path('getallproducts', views.getAllProduct, name='getallproducts'), # get all products url
+    path('getproductbycategory/<str:category>', views.getProductByCategory, name='getproductbycategory'), # get product by category url
+    path('getallcategory', views.getAllCategory, name='getallcategory'), # get all category url
+    path('getallproductcategorydata', views.allProductCategoryData, name='getallproductcategorydata'), # get all product and category data url
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 from dashboard.serializers import CategorySerializer, ProductSerializer
 
+from .models import Category, Product
 
 # test view for dashboard
 def dashboard(request):
@@ -22,7 +23,23 @@ def postproduct(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+# get view for all product
+@api_view(['GET'])
+def getAllProduct(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+# get view for product by category
+@api_view(['GET'])
+def getProductByCategory(request, category):
+    if request.method == 'GET':
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
 # post view for category
 @api_view(['POST'])
 def postcategory(request):
@@ -33,3 +50,24 @@ def postcategory(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# get view for all category
+@api_view(['GET'])
+def getAllCategory(request):
+    if request.method == 'GET':
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data)
+
+# get view for product and category
+@api_view(['GET'])
+def allProductCategoryData(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        category = Category.objects.all()
+        product_serializer = ProductSerializer(products, many=True)
+        category_serializer = CategorySerializer(category, many=True)
+        return Response({'products': product_serializer.data, 'category': category_serializer.data})
+    
+    
